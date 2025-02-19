9160a5deed28c3117c7d756fb1a217a506cd6498
DominikPyrek <pyrekdominik2025@gmail.com>
2025-02-19 13:35:07 +0100

    Zadanie nr1 :)

---
+++ Zadanie nr1 python/Zadanie_1.py
@@ -0,0 +1,118 @@
+import math
+import datetime 
+import random
+
+def pierwsza_niedziela_miesiaca(rok, miesiac):
+    for dzien in range(1, 8):
+        if datetime.datetime(rok, miesiac, dzien).weekday() == 6:
+            return dzien
+    return None
+
+
+expenses = {
+    "2023-01": {
+        "01": {
+            "food": [22.11, 43, 11.72, 2.2, 36.29, 2.5, 19],
+            "fuel": [210.22]
+        },
+        "09": {
+            "food": [11.9],
+            "fuel": [190.22]
+        }
+    },
+    "2023-03": {
+        "07": {
+            "food": [20, 11.9, 30.20, 11.9]
+        },
+        "04": {
+            "food": [10.20, 11.50, 2.5],
+            "fuel": []
+        }
+    },
+    "2023-04": {}
+}
+
+def solution1(expenses):
+    mediany = {}
+    for data in expenses:
+        wydatki = []
+
+        rok, miesiac = map(int, data.split('-'))
+
+        pierwsza_niedziela = pierwsza_niedziela_miesiaca(rok, miesiac)
+
+        if pierwsza_niedziela == None:
+            mediany[data] = None
+            continue
+        
+        for dzien in range(1, pierwsza_niedziela + 1):
+            dzien_klucz = "0"+str(dzien)
+            if dzien_klucz in expenses[data]:
+                for kategoria in expenses[data][dzien_klucz]:
+                    wydatki.extend(expenses[data][dzien_klucz][kategoria])
+        
+        ilosc_wydatkow = len(wydatki)
+        if ilosc_wydatkow == 0:
+            mediany[data] = None
+        else:
+            wydatki = sorted(wydatki)
+            x = ilosc_wydatkow // 2
+            if ilosc_wydatkow % 2 == 1:
+                mediany[data] = wydatki[x]
+            else:
+                mediany[data] = ((wydatki[x - 1] + wydatki[x]) / 2)
+    return mediany
+
+def quickselect(arr, k):
+    if not arr:
+        return None
+    pivot = random.choice(arr)
+    lows = [x for x in arr if x < pivot]
+    highs = [x for x in arr if x > pivot]
+    pivots = [x for x in arr if x == pivot]
+    if k < len(lows):
+        return quickselect(lows, k)
+    elif k < len(lows) + len(pivots):
+        return pivots[0]
+    else:
+        return quickselect(highs, k - len(lows) - len(pivots))
+ 
+'''
+    Zastosowałem w rozwiązaniu tego zadania quickselect ponieważ jest on szybszy od rozwiązania na nr1.
+    Rozwiązanie nr 1 ma złożoność O(n log n) natomiast quickselect O(n).
+    Quickselect ma również przewage względem zużycia pamięci.
+    Minusem tego algorytmu jest to że w najgorszym przypadku jego złożonośc wynosi O(n^2) oraz to że dla mniejszych zbiorów danych jest nie zawsze szybszy względem innych algorytmów.
+    Plusem jest również to że nie musimy sortować zbioru kiedy korzystamy z quickselect.
+'''
+def solution2(expenses):
+    mediany = {}
+    for data in expenses:
+        wydatki = []
+        rok, miesiac = map(int, data.split('-'))
+        pierwsza_niedziela = pierwsza_niedziela_miesiaca(rok, miesiac)
+        if pierwsza_niedziela == None:
+            mediany[data] = None
+            continue
+
+        for dzien in range(1, pierwsza_niedziela + 1):
+            dzien_klucz = "0"+str(dzien)
+            if dzien_klucz in expenses[data]:
+                for kategoria in expenses[data][dzien_klucz]:
+                    wydatki.extend(expenses[data][dzien_klucz][kategoria])
+        
+        ilosc_wydatkow = len(wydatki)
+
+        if ilosc_wydatkow == 0:
+            mediany[data] = None
+            continue
+        
+        if ilosc_wydatkow % 2 == 1:
+            mediana = quickselect(wydatki, ilosc_wydatkow // 2)
+        else:
+            mediana = ((quickselect(wydatki, (ilosc_wydatkow - 1 ) // 2) + quickselect(wydatki, ilosc_wydatkow // 2) )/ 2 )
+        mediany[data] = mediana
+    return mediany
+
+
+print(solution1(expenses))
+print(solution2(expenses))
